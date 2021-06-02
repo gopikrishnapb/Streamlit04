4b61b8eef18ebc77765891e86d2c99a8c72fa431
Gopikrishna <53814190+gopikrishnapb@users.noreply.github.com>
2021-06-02 23:23:10 +0530

    Add files via upload

---
+++ Assign4.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import streamlit as st
+st.title("Students Performance")
+st.header("Education")
+st.text("Dataset")
+data=pd.read_csv('Dataset.csv')
+st.dataframe(data)
+#math score of female students
+math=data[['math score','gender']]
+result = math.loc[math['gender'] == 'female']
+st.dataframe(result)
+st.subheader("Displot")
+sns.displot(data)
+st.pyplot()
+st.subheader("Jointplot")
+sns.jointplot(x='reading score',y='writing score',data=data,kind='hex')
+st.pyplot()
+st.baloon()
